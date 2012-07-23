9f52c5a54a185d331a5fb45b01b481a63e6c1bdc
T.A.Chappell@gmail.com <T.A.Chappell@gmail.com@d0433559-5a07-9fa1-c220-3d1c86df801f>
2012-07-23 01:57:56 +0000

    Added missing files

---
+++ src/topsig-stats.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "uthash.h"
+#include "topsig-global.h"
+#include "topsig-config.h"
+#include "topsig-stats.h"
+
+#define DOCSTATS_TERMLEN 32
+
+typedef struct {
+  char t[DOCSTATS_TERMLEN];
+  unsigned int freq_docs;
+  unsigned int freq_terms;
+  UT_hash_handle hh;
+} StatTerm;
+static int total_terms;
+static StatTerm *termtable = NULL;
+static StatTerm *termlist = NULL;
+
+int TermFrequencyStats(const char *term)
+{
+  if (termtable == NULL) return -1;
+  StatTerm *cterm;
+  HASH_FIND_STR(termtable, term, cterm);
+  return cterm->freq_terms;
+}
+
+void Stats_InitCfg()
+{
+  total_terms = 0;
+  char *termstats_path = Config("TERMSTATS-PATH");
+  if (termstats_path) {
+    FILE *fp = fopen(termstats_path, "rb");
+    fseek(fp, 0, SEEK_END);
+    int records = ftell(fp) / (32 + 4 + 4);
+    fseek(fp, 0, SEEK_SET);
+    termlist = malloc(sizeof(StatTerm) * records);
+    
+    int pips_drawn = -1;
+    printf("\n");
+    for (int i = 0; i < records; i++) {
+      fread(termlist[i].t, DOCSTATS_TERMLEN, 1, fp);
+      termlist[i].freq_docs = file_read32(fp);
+      termlist[i].freq_terms = file_read32(fp);
+      total_terms += termlist[i].freq_terms;
+      StatTerm *cterm = termlist + i;
+      
+      HASH_ADD_STR(termtable, t, cterm);
+
+      int pips = ((i + 1) * 10 + (records / 2)) / records;
+      if (pips > pips_drawn) {
+        pips_drawn = pips;
+        printf("\rReading term stats: [");
+        for (int p = 0; p < 10; p++) {
+          printf(p < pips ? "*" : " ");
+        }
+        printf("]");
+      }
+
+    }
+    printf("\n");
+    
+    fclose(fp);
+  }
+}
---
+++ src/topsig-stats.h
@@ -0,0 +1,8 @@
+#ifndef TOPSIG_STATS_H
+#define TOPSIG_STATS_H
+
+void Stats_InitCfg();
+
+int TermFrequencyStats(const char *);
+
+#endif
