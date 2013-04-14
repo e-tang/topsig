1bf3a251a2653d2842d3b1285881b56ef9ec0715
T.A.Chappell@gmail.com <T.A.Chappell@gmail.com@d0433559-5a07-9fa1-c220-3d1c86df801f>
2013-04-14 23:52:47 +0000

    ISSL source files added

---
+++ trunk/src/topsig-issl.h
@@ -0,0 +1,10 @@
+#ifndef TOPSIG_ISL_H
+#define TOPSIG_ISL_H
+
+void RunCreateISL();
+void RunSearchISL();
+void RunSearchISLTurbo();
+
+void ExperimentalRerankTopFile();
+
+#endif /* TOPSIG_ISL_H */
